00d07fb4affa43a5f620c31788c3535a2c33d4a4
Leonardo Capillera <lcapillera@gmail.com>
2022-08-31 07:20:45 +0200

    Init.lua

---
+++ init.lua
@@ -0,0 +1,209 @@
+--[[ init.lua ]]
+
+-- LEADER
+-- These keybindings need to be defined before the first /
+-- is called; otherwise, it will default to "\"
+vim.g.mapleader = ","
+vim.g.localleader = "\\"
+
+-- IMPORTS
+-- require('vars')      -- Variables
+-- require('opts')      -- Options
+-- require('keys')      -- Keymaps
+-- require('plug')      -- Plugins
+
+-- PLUGINS
+
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+    packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+end
+
+require('packer').startup(function(use)
+  -- install all the plugins you need here
+
+  use 'tanvirtin/monokai.nvim'
+
+  -- the plugin manager can manage itself
+  use {'wbthomason/packer.nvim'}
+
+  -- lsp config for elixir-ls support
+  use {'neovim/nvim-lspconfig'}
+
+  -- cmp framework for auto-completion support
+  use {'hrsh7th/nvim-cmp'}
+
+  -- install different completion source
+  use {'hrsh7th/cmp-nvim-lsp'}
+  use {'hrsh7th/cmp-buffer'}
+  use {'hrsh7th/cmp-path'}
+  use {'hrsh7th/cmp-cmdline'}
+
+  -- you need a snippet engine for snippet support
+  -- here I'm using vsnip which can load snippets in vscode format
+  use {'hrsh7th/vim-vsnip'}
+  use {'hrsh7th/cmp-vsnip'}
+
+  -- treesitter for syntax highlighting and more
+  use {'nvim-treesitter/nvim-treesitter'}
+end)
+
+require('monokai').setup {
+  italics = false,
+  palette = {
+    base2 = '#131411'
+  }
+}
+-- require('monokai').setup { palette = require('monokai').pro }
+-- require('monokai').setup { palette = require('monokai').soda }
+-- require('monokai').setup { palette = require('monokai').ristretto }
+
+-- `on_attach` callback will be called after a language server
+-- instance has been attached to an open buffer with matching filetype
+-- here we're setting key mappings for hover documentation, goto definitions, goto references, etc
+-- you may set those key mappings based on your own preference
+local on_attach = function(client, bufnr)
+  local opts = { noremap=true, silent=true }
+
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>cr', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>cf', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>cd', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+end
+
+-- setting up the elixir language server
+-- you have to manually specify the entrypoint cmd for elixir-ls
+
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+require('lspconfig').elixirls.setup {
+  cmd = { "/Users/leo/.cache/nvim/lspconfig/elixirls/elixir-ls/release/language_server.sh" },
+  on_attach = on_attach,
+  capabilities = capabilities
+}
+
+local cmp = require'cmp'
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      -- setting up snippet engine
+      -- this is for vsnip, if you're using other
+      -- snippet engine, please refer to the `nvim-cmp` guide
+      vim.fn["vsnip#anonymous"](args.body)
+    end,
+  },
+  mapping = {
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+  },
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'vsnip' }, -- For vsnip users.
+    { name = 'buffer' }
+  })
+})
+
+-- helper functions
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local feedkey = function(key, mode)
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+end
+
+cmp.setup({
+  -- other settings ...
+  mapping = {
+    -- other mappings ...
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+  cmp.select_next_item()
+      elseif vim.fn["vsnip#available"](1) == 1 then
+  feedkey("<Plug>(vsnip-expand-or-jump)", "")
+      elseif has_words_before() then
+  cmp.complete()
+      else
+  fallback()
+      end
+    end, { "i", "s" }),
+    ["<S-Tab>"] = cmp.mapping(function()
+      if cmp.visible() then
+  cmp.select_prev_item()
+      elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+  feedkey("<Plug>(vsnip-jump-prev)", "")
+      end
+    end, { "i", "s" })
+  }
+})
+
+
+-- require'nvim-treesitter.configs'.setup {
+--   ensure_installed = "maintained",
+--   sync_install = false,
+--   ignore_install = { },
+--   highlight = {
+--     enable = true,
+--     disable = { }
+--   },
+-- }
+
+require'nvim-treesitter.configs'.setup {
+  -- A list of parser names, or "all"
+  ensure_installed = { "c", "lua", "rust", "elixir" },
+
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = false,
+
+  -- Automatically install missing parsers when entering buffer
+  auto_install = true,
+
+  -- List of parsers to ignore installing (for "all")
+  ignore_install = { "javascript" },
+
+  ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+  -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+
+  highlight = {
+    -- `false` will disable the whole extension
+    enable = true,
+
+    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+    -- the name of the parser)
+    -- list of language that will be disabled
+    disable = { "c", "rust" },
+
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+  },
+}
+
+-- require('nvim-treesitter.configs').setup {
+--   ensure_installed = { "lua", "rust", "toml" },
+--   auto_install = true,
+--   highlight = {
+--     enable = true,
+--     additional_vim_regex_highlighting=false,
+--   },
+--   ident = { enable = true },
+--   rainbow = {
+--     enable = true,
+--     extended_mode = true,
+--     max_file_lines = nil,
+--   }
+-- }
