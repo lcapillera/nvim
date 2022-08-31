1cac4439444f97edcde8c53b2da5dcef7d667771
Leonardo Capillera <lcapillera@gmail.com>
2022-08-31 08:46:46 +0200

    Add keys mapping

--- init.lua
+++ init.lua
@@ -9,7 +9,7 @@ vim.g.localleader = "\\"
 -- IMPORTS
 require('vars')      -- Variables
 -- require('opts')      -- Options
--- require('keys')      -- Keymaps
+require('keys')      -- Keymaps
 require('plug')      -- Plugins
 
 -- PLUGINS
@@ -54,33 +54,34 @@ require('mason-lspconfig').setup{}
 -- instance has been attached to an open buffer with matching filetype
 -- here we're setting key mappings for hover documentation, goto definitions, goto references, etc
 -- you may set those key mappings based on your own preference
-local on_attach = function(client, bufnr)
-  local opts = { noremap=true, silent=true }
+-- local on_attach = function(client, bufnr)
+--   local opts = { noremap=true, silent=true }
 
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>cr', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>cf', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>cd', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
-end
+--   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+--   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+--   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+--   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+--   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+--   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+--   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>cr', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+--   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+--   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>cf', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+--   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>cd', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+--   vim.api.nvim_buf_set_keymap(bufnr, 'n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+--   vim.api.nvim_buf_set_keymap(bufnr, 'n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+-- end
 
 -- setting up the elixir language server
 -- you have to manually specify the entrypoint cmd for elixir-ls
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+-- local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-require('lspconfig').elixirls.setup {
-  cmd = { "/Users/leo/.cache/nvim/lspconfig/elixirls/elixir-ls/release/language_server.sh" },
-  on_attach = on_attach,
-  capabilities = capabilities
-}
+-- require('lspconfig').elixirls.setup {
+--   cmd = { "/Users/leo/.cache/nvim/lspconfig/elixirls/elixir-ls/release/language_server.sh" },
+--   on_attach = on_attach,
+--   capabilities = capabilities
+-- }
+require('lspconfig').elixirls.setup{}
 
 local cmp = require'cmp'
 
@@ -149,42 +150,8 @@ cmp.setup({
 --   },
 -- }
 
--- require'nvim-treesitter.configs'.setup {
---   -- A list of parser names, or "all"
---   ensure_installed = { "c", "lua", "rust", "elixir" },
-
---   -- Install parsers synchronously (only applied to `ensure_installed`)
---   sync_install = false,
-
---   -- Automatically install missing parsers when entering buffer
---   auto_install = true,
-
---   -- List of parsers to ignore installing (for "all")
---   ignore_install = { "javascript" },
-
---   ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
---   -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
-
---   highlight = {
---     -- `false` will disable the whole extension
---     enable = true,
-
---     -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
---     -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
---     -- the name of the parser)
---     -- list of language that will be disabled
---     disable = { "c", "rust" },
-
---     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
---     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
---     -- Using this option may slow down your editor, and you may see some duplicate highlights.
---     -- Instead of true it can also be a list of languages
---     additional_vim_regex_highlighting = false,
---   },
--- }
-
 require('nvim-treesitter.configs').setup {
-  ensure_installed = { "lua", "rust", "toml" },
+  ensure_installed = { "lua", "rust", "toml", "elixir" },
   auto_install = true,
   highlight = {
     enable = true,
--- lua/keys.lua
+++ lua/keys.lua
@@ -15,18 +15,18 @@ map('n', '<leader>f', [[:Telescope live_grep<CR>]], {})
 map('n', '<leader>b', [[:Telescope buffers<CR>]], {})
 
 -- Vim inspector
-vim.cmd([[
-    nmap <F9> <cmd>call vimspector#Launch()<cr>
-    nmap <F5> <cmd>call vimspector#StepOver()<cr>
-    nmap <F8> <cmd>call vimspector#Reset()<cr>
-    nmap <F11> <cmd>call vimspector#StepOver()<cr>")
-    nmap <F12> <cmd>call vimspector#StepOut()<cr>")
-    nmap <F10> <cmd>call vimspector#StepInto()<cr>")
-]])
+-- vim.cmd([[
+--     nmap <F9> <cmd>call vimspector#Launch()<cr>
+--     nmap <F5> <cmd>call vimspector#StepOver()<cr>
+--     nmap <F8> <cmd>call vimspector#Reset()<cr>
+--     nmap <F11> <cmd>call vimspector#StepOver()<cr>")
+--     nmap <F12> <cmd>call vimspector#StepOut()<cr>")
+--     nmap <F10> <cmd>call vimspector#StepInto()<cr>")
+-- ]])
 
-map('n', "Db", ":call vimspector#ToggleBreakpoint()<cr>", {})
-map('n', "Dw", ":call vimspector#AddWatch()<cr>", {})
-map('n', "De", ":call vimspector#Evaluate()<cr>", {})
+-- map('n', "Db", ":call vimspector#ToggleBreakpoint()<cr>", {})
+-- map('n', "Dw", ":call vimspector#AddWatch()<cr>", {})
+-- map('n', "De", ":call vimspector#Evaluate()<cr>", {})
 
 -- FloaTerm configuration
 map('n', "<leader>ft", ":FloatermNew --name=myfloat --height=0.8 --width=0.7 --autoclose=2 fish <CR> ", {})
